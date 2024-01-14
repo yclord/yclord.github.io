d593a12a2e3c37d58ae0babb2656d27c93a814e6
yclord <155416663+yclord@users.noreply.github.com>
2024-01-14 07:03:58 +0000

    Deploying to main from @ yclord/yclord.github.io@bd42000d9129f8516c9d80528c65e4738346248c 🚀

--- src/js/audio.js
+++ src/js/audio.js
@@ -1,22 +1,22 @@
-var isPlaying = false;
+var icount = 0;
 var play_audio = function(id, audio_path){
   let ele = document.getElementById(id);
-  if (ele && !isPlaying){
-    isPlaying = true;
+  if (ele && icount < 3){
+    icount += 1;
     ele.style.backgroundColor = 'orange';
     let audio = new Audio(audio_path);
     audio.addEventListener(
       'ended', 
       function(){
         ele.style.backgroundColor = 'rgba(0,0,0,0)';
-        isPlaying = false;
+        icount -= 1;
       }
     );
     audio.addEventListener(
       'error', 
       function(){
         ele.style.backgroundColor = 'rgba(0,0,0,0)';
-        isPlaying = false;
+        icount -= 1;
       }
     );
     audio.play();
